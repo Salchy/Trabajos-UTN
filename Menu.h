273050c7e4483387107de68db5c30a59577cce1d
Salchy <65300209+Salchy@users.noreply.github.com>
2024-10-28 21:18:18 -0300

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+class Menu {
+    public:
+        // Constructor:
+        Menu(const std::string tittle);
+
+        // Métodos:
+        bool addItem(const std::string itemName);
+        void showMenu();
+        int getSelection();
+
+        // Destructor:
+        ~Menu() {
+            delete[] _items;
+        }
+    private:
+        std::string _tittle;
+        std::string * _items = nullptr;
+        int _itemsCount;
+        int _selectedItem;
+
+        int getMaxCharsOfOptions() const;
+        std::string * getMenuItems() const;
+        int getItemsCount() const;
+        std::string getTitle() const;
+        int getSelectedItem() const;
+        void traverseMenu(int key);
+};
